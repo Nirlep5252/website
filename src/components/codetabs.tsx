245e0c6de04fad1eae836480698a5270708d1553
Nirlep <70529587+Nirlep5252@users.noreply.github.com>
2023-05-04 20:49:56 +0530

    Added increasing-array

--- src/components/codetabs.tsx
+++ src/components/codetabs.tsx
@@ -4,13 +4,9 @@ export default function CodeTabs(props: {
   children: React.ReactNode;
   langs: string[];
 }) {
-  console.log(props.langs);
-
   ("use client");
   const [active, setActive] = React.useState(0);
   const codes = React.Children.toArray(props.children);
-
-  console.log(codes);
 
   ("use server");
   return (
@@ -18,10 +14,9 @@ export default function CodeTabs(props: {
       <div className="code-tabs-container">
         <div className="code-tabs flex glassmorphism w-fit rounded-full">
           {props.langs.map((lang) => {
-            console.log(props.langs)
             return (
               <>
-                <div onClick={() => {
+                <div key={lang} onClick={() => {
                   setActive(props.langs.indexOf(lang));
                 }} className={`hover-glassmorphism border cursor-pointer py-1.5 px-3 rounded-full ${props.langs[active] === lang ? "glassmorphism border-gray-200" : ""}`}>
                   {lang}
